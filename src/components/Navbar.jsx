a530b20db3adf22af14366e371d395188eab0382
Ritik-17 <ritikhutnarayan@gmail.com>
2023-08-11 19:49:10 +0530

    a shopping cart webiste

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import {FaShoppingCart} from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <div>
+      <img/>
+      <div>
+        <p>Home</p>
+        <FaShoppingCart/>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
