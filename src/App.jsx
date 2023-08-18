af2337611f76cdf424e37ed7d86875ce8de0eccf
Ritik-17 <ritikhutnarayan@gmail.com>
2023-08-18 19:33:34 +0530

    Added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Cart from "./pages/Cart";
 const App = () => {
   return (
     <div>
-      <div>
-        <Navbar/>
+      <div  className="bg-slate-900">
+        <Navbar />
       </div>
       <Routes>
           <Route path="/" element={<Home/>}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,16 +28,16 @@ const Home = () => {
 
 
   return (
-    <div>
+    <div >
       {loading ? <Spinner/> : 
       posts.length > 0 ? (
-        <div>
+        <div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
           {posts.map((post) => (
             <Product key={post.id} post={post} />
       ))}
         </div>
       ) : 
-        <div>
+        <div className="flex justify-center items-center">
           No Data Found
         </div>
        }
